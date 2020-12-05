964680244bf84c104c40fc7bb20c4abbeabac956
MichaelWarner32 <warner.mike32@gmail.com>
2020-12-05 15:42:13 +0000

    Create the flask app

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "GmmXHiSge9SmX9y7I88qRpx5k96t7Zef")
+os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_DBNAME", "task_manager")
