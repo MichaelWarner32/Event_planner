df53a0f5d1afda6dba0fd6a15958f60755a69207
MichaelWarner32 <warner.mike32@gmail.com>
2020-12-20 16:44:31 +0000

    Added logout functionality. Also added what navbar tabs show for logged in and logged out users.

--- app.py
+++ app.py
@@ -74,6 +74,14 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/logout")
+def logout():
+    # log out the user
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
